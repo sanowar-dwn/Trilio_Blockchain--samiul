cfa1f545f24ecf60af6a30f814be8676bc8c3dd5
SamiulNahiyan <samiulnahiyan@gmail.com>
2022-12-27 22:54:21 +0600

    all files

---
+++ blockchain.py
@@ -0,0 +1,12 @@
+#blockchain
+
+class Blockchain:
+    def __init__(self, name="", difficulty=2, minimum_transactions=2):
+        self.name = name
+        self.difficulty = difficulty
+        self.minimum_transactions = minimum_transactions
+        self.chain = [
+        ]
+        self.pending_transactions = [
+
+        ]
